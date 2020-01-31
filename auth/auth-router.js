fb4626857588e40dc64882f01afddbf0d628205d
Emmanuel Tanifum <etantanifum@gmail.com>
2020-01-31 12:26:38 -0500

    dependencies in, readme answered and can registera user

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs");
+
+const Users = require("./auth-model.js");
+
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(200).json(saved)
+    })
+    .catch(err => {
+      res.status(500).json({ message: "try againnn lol"})
+    })
+
+
 });
 
 router.post('/login', (req, res) => {

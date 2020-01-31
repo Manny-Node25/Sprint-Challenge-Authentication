d002f2eb9b212140c52ff86f1306763f41678317
Emmanuel Tanifum <etantanifum@gmail.com>
2020-01-31 13:21:48 -0500

    minimum met, had to remodel and restructure files

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,59 +1,57 @@
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const router = require('express').Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const secrets = require("./secret")
-const Users = require("./auth-model.js");
+
+const { jwtSecret } = require('../config/secrets.js')
+
+const Users = require('../users/users-model.js');
 
 
 router.post('/register', (req, res) => {
-  // implement registration
   let user = req.body;
-  const hash = bcrypt.hashSync(user.password);
+  const hash = bcrypt.hashSync(user.password, 10); 
   user.password = hash;
 
   Users.add(user)
     .then(saved => {
-      res.status(200).json(saved)
+      res.status(201).json(saved);
     })
-    .catch(err => {
-      res.status(500).json({ message: "try againnn lol"})
-    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
-const generateToken = user => {
-  const payload = {
-    username: user.username,
-    id: user.id,
-    department: user.department
-  }
-  const options = {
-    expiresIn: "1d"
-  }
-
-  return jwt.sign(payload, secrets.jwtSecret, options)
-
-}
 
 router.post('/login', (req, res) => {
-  // implement login
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
-      // res.status(200).json({ message: "Haiii!!!"})
-        if (user && bcrypt.compareSync(password, user.password)) {
-            const token = generateToken(user);
-            res.status(200).json({
-                message: "Welcome to ...here lol!",
-                token
-            })
-        } else {
-            res.status(401).json({ message: "invalid credentials" })
-        }
+      if (user && bcrypt.compareSync(password, user.password)) {
+
+        const token = signToken(user); 
+        res.status(200).json({ token }); 
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
     })
     .catch(error => {
-        res.status(500).json({ message: "Don't worry. It aint u; it's me man"});
-    })
+      res.status(500).json(error);
+    });
 });
+
+
+function signToken(user) {
+  const payload = {
+    department: user.department
+  };
+
+  const options = {
+    expiresIn: '1d'
+  };
+
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
